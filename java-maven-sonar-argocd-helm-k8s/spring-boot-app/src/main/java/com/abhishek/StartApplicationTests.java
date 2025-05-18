a338d85e68c2cd4aca182e8b09fc46a668428782
Ajay <103913120+Ajay9865444085@users.noreply.github.com>
2025-05-18 15:40:41 +0530

    Create StartApplicationTests.java

---
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Basic test to ensure the Spring Boot context loads successfully
+    }
+}
